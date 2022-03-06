5f9977485cc16cf7146eed8a351cdcfc704a9bd6
Lia Lenckowski <lialenck@protonmail.com>
2022-03-06 16:10:29 +0100

    fix varint formatting, implement tests

--- src/Data/VarInt/Put.hs
+++ src/Data/VarInt/Put.hs
@@ -1,16 +1,28 @@
-module Data.VarInt.Put (putVarInt, formatVarInt) where
+module Data.VarInt.Put (formatVarInt, formatVarLong, putVarInt, putVarLong) where
 
 import Data.Binary.Put
 import Data.Bits
+import Data.Word
 
 import qualified Data.ByteString as BS
 
-formatVarInt :: Int -> BS.ByteString
-formatVarInt 0 = BS.empty
-formatVarInt n =
+formatVarWord64_ :: Word64 -> BS.ByteString
+formatVarWord64_ 0 = BS.empty
+formatVarWord64_ n =
     let more = shiftR n 7
         cur = fromIntegral $ n .&. 0x7f .|. (if more > 0 then 0x80 else 0)
-    in cur `BS.cons` formatVarInt more
+    in cur `BS.cons` formatVarWord64_ more
+
+formatVarWord64 :: Word64 -> BS.ByteString
+formatVarWord64 0 = BS.singleton 0
+formatVarWord64 n = formatVarWord64_ n
+
+formatVarLong :: Int -> BS.ByteString
+formatVarLong = formatVarWord64 . fromIntegral
+formatVarInt :: Int -> BS.ByteString
+formatVarInt n = formatVarWord64 $ fromIntegral (fromIntegral n :: Word32)
 
 putVarInt :: Int -> Put
 putVarInt = putByteString . formatVarInt
+putVarLong :: Int -> Put
+putVarLong = putByteString . formatVarLong
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,58 @@
+import Test.Hspec
+
+import Data.Binary.Put
+import Data.Binary.Strict.Get
+import Data.VarInt.Get
+import Data.VarInt.Put
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+
+testVarIntGet = do
+    describe "Data.VarInt.Get" $ parallel $ do
+        it "can parse non-negative VarInts" $ do
+           runGet parseVarIntLen (BS.pack [0]) `shouldBe` (Right (0, 1), BS.empty)
+           runGet parseVarIntLen (BS.pack [69]) `shouldBe` (Right (69, 1), BS.empty)
+           runGet parseVarIntLen (BS.pack [128, 1]) `shouldBe` (Right (128, 2), BS.empty)
+           runGet parseVarIntLen (BS.pack [69, 1, 2]) `shouldBe` (Right (69, 1), BS.pack [1, 2])
+
+        it "can parse negative VarInts" $ do
+            runGet parseVarInt (BS.pack [255, 255, 255, 255, 15]) `shouldBe` (Right (-1), BS.empty)
+
+            let bs = BS.pack [128, 128, 128, 128, 8]
+            runGet parseVarInt bs `shouldBe` (Right (-2147483648), BS.empty)
+
+        it "can parse non-negative VarLongs" $ do
+            runGet parseVarLongLen (BS.pack [128, 1]) `shouldBe` (Right (128, 2), BS.empty)
+
+            let bs = BS.pack $ take 8 (repeat 255) ++ [0x7F]
+            runGet parseVarLong bs `shouldBe` (Right (9223372036854775807), BS.empty)
+
+        it "can parse negative VarLongs" $ do
+            let bs = BS.pack $ take 9 (repeat 255) ++ [1, 69]
+            runGet parseVarLong bs `shouldBe` (Right (-1), BS.pack [69])
+
+testVarIntPut = do
+    describe "Data.VarInt.Put" $ parallel $ do
+        it "can format non-negative VarInts" $ do
+            runPut (putVarInt 0) `shouldBe` BL.pack [0]
+            runPut (putVarInt 69) `shouldBe` BL.pack [69]
+            runPut (putVarInt 128) `shouldBe` BL.pack [128, 1]
+
+        it "can format negative VarInts" $ do
+            runPut (putVarInt (-1)) `shouldBe` BL.pack [255, 255, 255, 255, 15]
+            runPut (putVarInt (-2147483648)) `shouldBe` BL.pack [128, 128, 128, 128, 8]
+
+        it "can format non-negative VarLongs" $ do
+            runPut (putVarLong 128) `shouldBe` BL.pack [128, 1]
+
+            let bs = BL.pack $ take 8 (repeat 255) ++ [0x7F]
+            runPut (putVarLong 9223372036854775807) `shouldBe` bs
+
+        it "can format negative VarLongs" $ do
+            let bs = BL.pack $ take 9 (repeat 255) ++ [1]
+            runPut (putVarLong (-1)) `shouldBe` bs
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    testVarIntGet
+    testVarIntPut
